00851accb4e3f52e78d8088edb6a15a1bc727df5
Anthony Collier <hello@acollier.dev>
2024-03-06 00:45:00 -0500

    refactor(ui): improve url analysis ui
    - improve error handling
    - display selected url in select component
    - don't display selected url in analysis response
    - truncate url strings that are too long to display

--- src/commands/message/analyze.ts
+++ src/commands/message/analyze.ts
@@ -3,14 +3,14 @@ import extractUrls from 'extract-urls';
 import {
   ApplicationCommandType,
   CommandContext,
+  ComponentSelectOption,
   ComponentType,
   SlashCommand,
-  SlashCreator,
-  ComponentSelectOption
+  SlashCreator
 } from 'slash-create/web';
 import { cancelButton, jumpToMessageButton, resultEmbedBuilder, urlButtons, urlSelectComponent } from '@/ui';
 import { validateUrl } from '@/lib/fetch';
-import { analyzeUrl } from '@/lib/urls';
+import { analyzeUrl, truncate } from '@/lib/urls';
 
 export default class AnalyzeMessageCommand extends SlashCommand {
   constructor(creator: SlashCreator) {
@@ -20,20 +20,26 @@ export default class AnalyzeMessageCommand extends SlashCommand {
     });
   }
 
+  async onError(err: Error, ctx: CommandContext) {
+    return ctx.send({ content: 'Check console for error', ephemeral: true });
+  }
+
   async run(ctx: CommandContext) {
     await ctx.defer(true);
 
     const messageContent = ctx.targetMessage!.content;
-    const urls = extractUrls(messageContent);
-    if (!urls) return ctx.send({ content: 'No valid URLs on this message.' });
+    const urls = [...new Set(extractUrls(messageContent))];
+    if (urls.length < 1) return ctx.editOriginal({ content: 'No valid URLs on this message.' });
 
     const component = urlSelectComponent;
     const options: ComponentSelectOption[] = [];
 
     for (const url of urls) {
+      const truncated = truncate(url, 100);
+
       options.push({
-        label: url,
-        value: url,
+        label: truncated,
+        value: truncated,
         description: 'The parsed URL result.'
       });
     }
@@ -66,11 +72,29 @@ export default class AnalyzeMessageCommand extends SlashCommand {
     });
   }
 
-  private async handleUrl(ctx: CommandContext, url: string) {
+  private async handleUrl(ctx: CommandContext, selectedUrl: string) {
+    const component = urlSelectComponent;
+    component.options = component.options!.map((opt) => {
+      if (opt.value === selectedUrl) {
+        return {
+          ...opt,
+          default: true
+        };
+      }
+      return {
+        ...opt,
+        default: false
+      };
+    });
+
     await ctx.editOriginal({
-      content: `Selected URL: \`\`\`${url}\`\`\``,
+      content: `Selected URL: \`\`\`${selectedUrl}\`\`\``,
       embeds: [],
       components: [
+        {
+          type: ComponentType.ACTION_ROW,
+          components: [component]
+        },
         {
           type: ComponentType.ACTION_ROW,
           components: [
@@ -86,49 +110,54 @@ export default class AnalyzeMessageCommand extends SlashCommand {
     });
 
     ctx.registerComponent('analyze_button', async (btnCtx) => {
-      try {
-        const validUrl = await validateUrl(url);
-        if (!validUrl) return ctx.send(`The following URL has no response: \`${url}\``);
+      // try {
+      component.options = component.options!.map((opt) => {
+        if (opt.default) {
+          return {
+            ...opt,
+            default: false
+          };
+        }
 
-        const data = await analyzeUrl({
-          creator: this.creator,
-          ctx,
-          url
-        });
+        return opt;
+      });
 
-        const embed = resultEmbedBuilder({
-          input: data.data,
-          resultId: data.id,
-          existed: data.existed
-        });
+      const validUrl = await validateUrl(selectedUrl);
+      if (!validUrl) return ctx.editOriginal(`The following URL has no response: \`${selectedUrl}\``);
 
-        return ctx.editOriginal({
-          embeds: [embed],
-          components: [
-            {
-              type: ComponentType.ACTION_ROW,
-              components: [urlSelectComponent]
-            },
-            {
-              type: ComponentType.ACTION_ROW,
-              components: [
-                jumpToMessageButton({
-                  guildId: ctx.guildID!,
-                  channelId: ctx.targetMessage!.channelID,
-                  messageId: ctx.targetMessage!.id
-                }),
-                cancelButton
-              ]
-            }
-          ]
-        });
-      } catch (e: any) {
-        console.log(e);
-        return ctx.send({
-          content: 'Check console for error.',
-          ephemeral: true
-        });
-      }
+      const data = await analyzeUrl({
+        creator: this.creator,
+        ctx,
+        url: selectedUrl
+      });
+
+      const embed = resultEmbedBuilder({
+        input: data.data,
+        resultId: data.id,
+        existed: data.existed
+      });
+
+      return ctx.editOriginal({
+        content: '',
+        embeds: [embed],
+        components: [
+          {
+            type: ComponentType.ACTION_ROW,
+            components: [component]
+          },
+          {
+            type: ComponentType.ACTION_ROW,
+            components: [
+              jumpToMessageButton({
+                guildId: ctx.guildID!,
+                channelId: ctx.targetMessage!.channelID,
+                messageId: ctx.targetMessage!.id
+              }),
+              cancelButton
+            ]
+          }
+        ]
+      });
     });
 
     ctx.registerComponent('cancel_button', async (btnCtx) => {
