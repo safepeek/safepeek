36f9b82830f6d2faa5f0f1591c5f9df0fd06e4b6
Anthony Collier <hello@acollier.dev>
2024-03-24 23:58:33 -0400

    refactor: update twitter link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,7 +8,7 @@ export const APP_VERSION: string = packageJson.version;
 export const APP_GITHUB: string = 'https://github.com/safepeek/safepeek';
 
 export const WEBSITE: string = 'https://safepeek.org';
-export const TWITTER: string = 'https://twitter.com/safepeek';
+export const TWITTER: string = 'https://twitter.com/safepeekbot';
 export const DISCORD_INVITE: string = 'https://discord.gg/2TvARX4Xwp';
 export const BOT_INVITE: string =
   'https://discord.com/oauth2/authorize?client_id=1208283559799029760&permissions=274878024704&scope=bot+applications.commands';
